d4e75a529c3ec9c5bdfc097fed3cb746d09d11ca
Preston Fitzgerald <prestonfitz@gmail.com>
2024-02-07 11:09:49 -0700

    Add project files.

---
+++ Cooking/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$("#btnSubmit").click(function () {
+    var totPrice = 0;
+
+    // validate the numbers before submitting
+    if ($("#numLessons").val() < 1) {
+        alert("Please enter at least 1 lesson")
+    }
+    else if ($("#lessonLength").val() < 0.5) {
+        alert("Please select at least half an hour for a lesson")
+    }
+    else {
+        totPrice = $("#numLessons").val() * $("#lessonLength").val() * $("#priceHour").val();
+
+        $("#totalPrice").attr("value", totPrice);
+    }
+
+
+})
